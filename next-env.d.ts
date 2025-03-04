5622db551195f2ad4099b457f2beb7e36ea1ccdc
sagastumesnet <edin@sagastumes.net>
2025-03-04 17:08:31 -0600

    Add files via upload

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 4c7bd23a480803b914367abbdfa6b801dcf51b59
