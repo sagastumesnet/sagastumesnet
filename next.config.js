88efe26290081b9cb70c3fabb9af728d3cd8292b
sagastumesnet <edin@sagastumes.net>
2025-03-04 18:09:22 -0600

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: 'export',
   images: {
-    domains: ['localhost'],
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
